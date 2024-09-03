83678069726d56bf06d3bfc46e6f0c697c85b2de
Klisman Evangelista <klismanevangelista777@gmail.com>
2024-09-03 01:38:17 -0500

    controller_routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/new'
+  get 'orders/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :events
+  resources :orders
 end
