98fa8637fe6118a4da47e0791738ad0271616163
Merly Anco <merly2257@gmail.com>
2024-09-04 19:52:47 -0700

    terminado view formulario

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,20 @@
 class Event < ApplicationRecord
+  validates :name, :description, :date, :location, :quantity, :ticket_price, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+  validates :ticket_price, numericality: { greater_than: 0 }
+  validate :date_cannot_be_in_the_past
+
+
   belongs_to :user
   has_many :orders
   has_one_attached :photo
+
+  private
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "no puede ser en el pasado")
+    end
+  end
+
 end
