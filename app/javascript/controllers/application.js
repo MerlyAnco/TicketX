e0a0a262090a2f8b38751cd0796a6a7091afc3e4
Merly Anco <merly2257@gmail.com>
2024-09-06 14:47:58 -0700

    fix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,10 +4,14 @@ import CounterTicketsController from "./counter_tickets_controller"
 import HomeController from "./home_controller"
 import CarouselCategoryController from "./carousel_category_controller"
 <<<<<<< HEAD
+<<<<<<< HEAD
 import SweetalertController from "./sweetalert_controller"
 =======
 import PaymentController from "./payment_controller"
 >>>>>>> 2b11c4a81a68b99c658d7bf7aaab25eae1ae6960
+=======
+import SweetalertController from "./sweetalert_controller"
+>>>>>>> 057d48c (terminado sweet alert)
 
 const application = Application.start()
 application.register('form', FormController)
@@ -15,10 +19,14 @@ application.register('counter-tickets', CounterTicketsController)
 application.register('home', HomeController)
 application.register('category-filter', CarouselCategoryController)
 <<<<<<< HEAD
+<<<<<<< HEAD
 application.register('sweetalert', SweetalertController)
 =======
 application.register('payment', PaymentController)
 >>>>>>> 2b11c4a81a68b99c658d7bf7aaab25eae1ae6960
+=======
+application.register('sweetalert', SweetalertController)
+>>>>>>> 057d48c (terminado sweet alert)
 
 // Configure Stimulus development experience
 application.debug = false
